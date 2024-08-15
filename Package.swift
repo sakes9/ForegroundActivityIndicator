87c1f053b4a2afb12050a4acccbe755915910456
sake <sake.std.co@gmail.com>
2024-08-15 21:16:26 +0900

    chore: プラットフォームを iOS17 に指定する

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "ForegroundActivityIndicator",
+    platforms: [
+        .iOS(.v17)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -19,5 +22,4 @@ let package = Package(
         .testTarget(
             name: "ForegroundActivityIndicatorTests",
             dependencies: ["ForegroundActivityIndicator"]),
-    ]
-)
+    ])
