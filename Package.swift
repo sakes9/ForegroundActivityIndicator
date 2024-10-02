4e76410572e6efa51142362c24879b7618323ebe
sake <sake.std.co@gmail.com>
2024-10-02 13:12:45 +0900

    chore: NVActivityIndicatorView を依存関係に追加する

--- Package.swift
+++ Package.swift
@@ -12,13 +12,17 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "ForegroundActivityIndicator",
-            targets: ["ForegroundActivityIndicator"]),
+            targets: ["ForegroundActivityIndicator"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/ninjaprox/NVActivityIndicatorView.git", from: "5.2.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "ForegroundActivityIndicator"),
+            name: "ForegroundActivityIndicator",
+            dependencies: ["NVActivityIndicatorView"]),
         .testTarget(
             name: "ForegroundActivityIndicatorTests",
             dependencies: ["ForegroundActivityIndicator"]),
